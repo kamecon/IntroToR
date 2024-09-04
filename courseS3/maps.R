456d04b0d3d4daf9d592477af42fde92719dfd0f
Kamal Romero Sookoo <romerka@delta.europa.eu>
2024-09-04 20:22:13 +0200

    add sf map

--- courseS3/maps.R
+++ courseS3/maps.R
@@ -24,6 +24,187 @@ ggplot() +
                color="black")
 
 
+pacman::p_load(eurostat, sf, ggplot2, giscoR, dplyr)
+
+eur21 <- get_eurostat_geospatial(resolution = 10,
+                                 nuts_level = 2,
+                                 year = 2021)
+eu_countries
+eur21_2 <- inner_join(x = eu_countries,
+                      y = eur21,
+                      by = c("code" = "CNTR_CODE")) %>% 
+  st_as_sf()
+
+ggplot(data = eur21_2 %>% dplyr::filter(name == "Italy")) +
+  geom_sf()
+
+unemp <- get_eurostat(id = "tgs00010", select_time = "Y")
+
+unemp %>% filter(TIME_PERIOD == "2021-01-01" & sex == "T")
+
+eur21_3 <- inner_join(x = eur21_2,
+                      y = unemp %>% filter(TIME_PERIOD == "2021-01-01" & sex == "T" & isced11 == "TOTAL"),
+                      by = c("NUTS_ID" = "geo")) %>% 
+  st_as_sf()
+
+
+eur21_3 %>% 
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf()
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank())
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank())
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_continuous(type = "viridis")
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_continuous(type = "viridis", name = "Unemployment (%)")
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient(low = "yellow", high = "red", name = "Unemployment (%)")
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient2(low = "yellow", high = "red",mid = "orange", name = "Unemployment (%)")
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient2(low = "yellow",
+                       high = "red",
+                       mid = "orange",
+                       midpoint = 12,
+                       name = "Unemployment (%)")
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  select(values) %>% 
+  summarise(max(values))
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient2(low = "yellow",
+                       high = "red",
+                       mid = "orange",
+                       midpoint = 10,
+                       name = "Unemployment (%)")
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient2(low = "yellow",
+                       high = "red",
+                       mid = "orange",
+                       midpoint = 10,
+                       name = "Unemployment (%)") +
+  geom_sf_text(aes(label = NAME_LATN))
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient2(low = "yellow",
+                       high = "red",
+                       mid = "orange",
+                       midpoint = 10,
+                       name = "Unemployment (%)") +
+  geom_sf_text(aes(label = NAME_LATN), size =2)
+
+pacman::p_load(ggrepel)
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values, label= NAME_LATN)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient2(low = "yellow",
+                       high = "red",
+                       mid = "orange",
+                       midpoint = 10,
+                       name = "Unemployment (%)") +
+  geom_text_repel(aes(geometry = geometry),
+                  stat = "sf_coordinates")
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values, label= NAME_LATN)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient2(low = "yellow",
+                       high = "red",
+                       mid = "orange",
+                       midpoint = 10,
+                       name = "Unemployment (%)") +
+  geom_text_repel(aes(geometry = geometry),
+                  stat = "sf_coordinates",
+                  max.overlaps = Inf,
+                  box.padding = 0.5)
+
+eur21_3 %>%
+  dplyr::filter(name == "Italy") %>% 
+  ggplot(aes(fill = values, label= NAME_LATN)) +
+  geom_sf() +
+  theme(axis.text = element_blank()) +
+  theme(axis.ticks = element_blank()) +
+  scale_fill_gradient2(low = "yellow",
+                       high = "red",
+                       mid = "orange",
+                       midpoint = 10,
+                       name = "Unemployment (%)") +
+  geom_text_repel(aes(geometry = geometry),
+                  stat = "sf_coordinates",
+                  max.overlaps = Inf,
+                  box.padding = 0.5, size =4)
+
+
+eur21_3 %>% 
+  select(NUTS_ID, values)
+
 
 
 
