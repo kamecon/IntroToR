d434e48f6632494ff17dd2358aaaabb41b88b2db
KAMAL ANTONIO ROMERO SOOKOO <kamalantonio.romero@alu.uclm.es>
2024-09-18 07:42:29 +0200

    add libraries script

---
+++ courseS3/libraries.R
@@ -0,0 +1,16 @@
+
+install.packages("pacman")
+
+pacman::p_load(readxl, haven, writexl, dplyr, tidyr, AER, purrr)
+
+pacman::p_load(ggplot2, ggridges, ggthemes, maps, sf, giscoR, eurostat,
+               ggrepel, mapproj)
+
+pacman::p_load(wooldridge, estimatr, lmtest, sandwich, jtools,
+               broom, performance, stargazer)
+
+pacman::p_load(mfx, margins, plm, fixest, causaldata, modelsummary, rdrobust,
+               ivreg, see)
+
+
+
